34d1385e2098a778f45a897b5f5605210d8a4280
HanhNguyen319 <nguyen.thuyhanh319@gmail.com>
2021-09-07 18:11:15 +0700

    Commit New project

---
+++ Test Suites/createFile.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>createFile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9bb83351-a30c-496b-b3a6-005ee909c539</testSuiteGuid>
+   <testCaseLink>
+      <guid>af58fbd3-b2be-4fa5-a773-c1f652709f45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/autoCreateFile</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f6a7d134-0747-475d-ab06-d70c529a1a51</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/geneticaID</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>f6a7d134-0747-475d-ab06-d70c529a1a51</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>geneticaID</value>
+         <variableId>15c4854a-4e3a-4f5c-8ebc-52c856bfd4ee</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
